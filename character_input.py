8a582f8b2e66179963a6441552102f559e4427c7
THSPSS <sunyeongbang98@gmail.com>
2026-04-30 22:48:36 +0900

    chracter input with orders to write a code

---
+++ character_input.py
@@ -0,0 +1,11 @@
+name = input("Enter your name : ")
+age = input("Enter your age : ")
+
+#convert string into integer :best way to convert string to integer
+
+#1. calculate years left until 100
+# 100 - age
+
+#2. get current year and add result from 1
+
+#3. show user the result using print
