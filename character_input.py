019711468a918e93ede8e0ac739d91889db21460
THSPSS <sunyeongbang98@gmail.com>
2026-05-01 11:28:43 +0900

    add while loop to check user age input if it has characters

--- character_input.py
+++ character_input.py
@@ -1,11 +1,28 @@
-name = input("Enter your name : ")
-age = input("Enter your age : ")
+from datetime import datetime
+current_year = datetime.now().year
+gretting_and_continue = input("Hello! We will see year that you turn 100!\nPlease Enter if you want to continue:)")
+is_contain_char = True
+if gretting_and_continue=="":
+    age = "0"
+    name = input("Enter your name : ")
 
-#convert string into integer :best way to convert string to integer
 
-#1. calculate years left until 100
-# 100 - age
+    #convert string into integer :best way to convert string to integer
+    while is_contain_char:
+        age = input("Enter your age : ")
+        try:
+            int_age = int(age)
+            is_contain_char = False
+        except ValueError:
+            print("Please enter only number")
 
-#2. get current year and add result from 1
+    #1. calculate years left until 100
+    # 100 - age
+    years_left = 100 - int_age
 
-#3. show user the result using print
+    #2. get current year and add result from 1
+    result = current_year + years_left
+
+
+    #3. show user the result using print
+    print(f"{name} ! You become 100 years in {result}! :)")
