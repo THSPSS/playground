97e6dcd0e9af7d3418684391e8290cc945bb39a2
THSPSS <sunyeongbang98@gmail.com>
2026-05-04 20:33:18 +0900

    update list less than ten with using one-liner

---
+++ list_less_than_ten.py
@@ -0,0 +1,6 @@
+#how about put random numbers in list that would be more fun
+# a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+#for loop to check all the element in list
+#check if element is less than 5
+print([num for num in [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89] if num < 5])
+
