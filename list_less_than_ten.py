9811202bbfaf7c5136ae3ab1e294469d37d7a5a3
THSPSS <sunyeongbang98@gmail.com>
2026-05-05 14:47:37 +0900

    update with using functions from other file

--- list_less_than_ten.py
+++ list_less_than_ten.py
@@ -1,8 +1,32 @@
 import random
+
+from odd_or_even import asking_user_and_getting_data, convert_string_into_int
+
+
 #how about put random numbers in list that would be more fun
-a = [random.randint(1,45) for _ in range(11)]
-print(a)
+
 #for loop to check all the element in list
 #check if element is less than 5
-print([num for num in a if num < 5])
+def main():
+    user_number = None
+    a = [random.randint(1, 45) for _ in range(11)]
+    result_list = []
+
+
+    while user_number is None:
+        user_number = asking_user_and_getting_data("Please enter a number : ")
+
+    int_user_number = convert_string_into_int(user_number)
+
+    for num in a :
+        if num < int_user_number :
+            print(num)
+            result_list.insert(num)
+
+    print([num for num in a if num < 5])
+    print(result_list)
+
+
+if __name__ == "__main__":
+    main()
 
