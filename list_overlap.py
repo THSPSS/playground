ec645c178bdb7e84b59f70ff046f47a498e57979
THSPSS <sunyeongbang98@gmail.com>
2026-05-10 22:21:18 +0900

    check the lnegth of list

--- list_overlap.py
+++ list_overlap.py
@@ -3,9 +3,13 @@ b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
 result_list = []
 
 def main():
-    for num in a :
-        if num in b:
+    list_to_check = a if len(a) > len(b) else b
+    comparing_list = b if len(b) < len(a) else a
+    for num in list_to_check :
+        if num in comparing_list:
             result_list.append(num)
+
+    print(result_list)
     # for num in b:
     #     if num in a:
     #         #check if it is there or not
