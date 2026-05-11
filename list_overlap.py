a98b8bfdec99ef9a30fc163b39abf5d448bd833c
THSPSS <sunyeongbang98@gmail.com>
2026-05-11 20:13:03 +0900

    update list_overlap with random and for loop only one time

--- list_overlap.py
+++ list_overlap.py
@@ -1,18 +1,17 @@
-a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
-b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+import random
+
+a = [random.randint(1,40) for _ in range(random.randint(10,15))]
+b = [random.randint(1,40) for _ in range(random.randint(10,15))]
+print(a,b)
 result_list = []
 
 def main():
-    list_to_check = a if len(a) > len(b) else b
-    comparing_list = b if len(b) < len(a) else a
-    for num in list_to_check :
-        if num in comparing_list:
+    for num in a :
+        if num in b and num not in result_list:
             result_list.append(num)
 
+
     print(result_list)
-    # for num in b:
-    #     if num in a:
-    #         #check if it is there or not
 
 
 if __name__ == "__main__":
