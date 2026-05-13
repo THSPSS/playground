02d08ba25aeb8241362df41e5101cc4f217556b8
THSPSS <sunyeongbang98@gmail.com>
2026-05-13 20:36:53 +0900

    update string lists

--- string_lists.py
+++ string_lists.py
@@ -1,6 +1,18 @@
+from odd_or_even import asking_user_and_getting_data
 
+a = [1,2,3]
+backword_string_list = []
 def main():
-    print("string lists")
+    user_string = asking_user_and_getting_data("Please Enter String : ")
+    print(user_string == user_string[::-1])
+    # start from end and to start
+    # for i in range(len(user_string)-1,-1,-1):
+    #     backword_string_list.append(user_string[i])
+    #     # backword_string_list.append(a[i-1])
+    # print(backword_string_list)
+    # if user_string == ''.join(map(str, backword_string_list)):
+    #     print(f"{user_string} is a palindrome!")
+
 
 if __name__ == "__main__" :
     main()
