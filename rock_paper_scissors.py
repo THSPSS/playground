0240a1d08f8b39f28e43ea948424018842d76e7e
THSPSS <sunyeongbang98@gmail.com>
2026-05-19 23:09:19 +0900

    update rock paper scissors

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,6 +1,7 @@
 import random
 
-outcomes = ['r' , 'p' , 's']
+other_player_outcomes = ['R' , 'P' , 'S']
+
 
 def main():
     game_is_on = True
@@ -9,44 +10,32 @@ def main():
         player_choice = input("Player choose among rock, paper, scissors using R for rock, P for paper and S for Scissors : ").upper()
     #ask first player to choose among rock paper scissors
     #keep input and ask other play choose among rock , paper, scissors
-        other_player_choice = input("Other player choose among rock, paper, scissors using R for rock, P for paper and S for Scissors : ").upper()
+        other_player_choice = random.choice(other_player_outcomes)
+        print(other_player_choice)
     #compare users choices
-        if player_choice == "R":
+        if player_choice == other_player_choice:
+            print("It is a draw!")
+        elif player_choice == 'R':
             if other_player_choice == "S":
-                winner = "first player"
-            elif other_player_choice == "P":
-                winner = "other player"
+                print("You Win!")
             else:
-                winner = None
+                print("you Lose!")
         elif player_choice == "P" :
             if other_player_choice == "R":
-                winner = "first player"
-            elif other_player_choice == "S":
-                winner = "other player"
+                print("You Win!")
             else:
-                winner = None
-        else :
-            if other_player_choice == "R":
-                winner = "other player"
-            elif other_player_choice == "P":
-                winner = "first player"
+                print("you Lose!")
+        elif player_choice == "S":
+            if other_player_choice == "P":
+                print("You Win!")
             else:
-                winner = None
-
-        if winner is None:
-            print("It is a draw!")
-        elif winner == "first player":
-            print("winner is first player")
+                print("you Lose!")
         else:
-            print("winner is other player")
-
+            print("Invalid input! please enter again")
         another_round = input("Do you want to play other round? (Y/N) : ").upper()
         if another_round == "N":
             game_is_on = False
 
-        #pick winner
-
-        #ask user about keep game on
 
 
 if __name__ == "__main__":
