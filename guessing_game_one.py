2812a740dbc8eec875f1c0fba0caecef6c88ffbd
THSPSS <sunyeongbang98@gmail.com>
2026-05-22 20:26:37 +0900

    update using random number

---
+++ guessing_game_one.py
@@ -0,0 +1,25 @@
+import random
+
+def main():
+    count_the_guesses = 0
+    game_is_on = True
+    the_number = random.randint(1, 9)
+    while game_is_on :
+        #generate random number between 1 and 9
+        print(the_number)
+        #ask user to guess the number
+
+        #check answer and compare with the number
+
+        #check if it is same
+        #then return
+
+        #else if it is less then the number
+        #print guess number with upward
+
+        #else say lower the guessing number
+
+
+
+if __name__ == "__main__":
+    main()
