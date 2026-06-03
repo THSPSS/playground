93e7de16e42a0ffeb75057b1ab445d030e719fd8
THSPSS <sunyeongbang98@gmail.com>
2026-06-03 09:52:46 +0900

    update Guessing Game One with While and if pattern

--- guessing_game_one.py
+++ guessing_game_one.py
@@ -1,23 +1,38 @@
 import random
 
+from odd_or_even import convert_string_into_int
+
+
 def main():
     count_the_guesses = 0
     game_is_on = True
+    # generate random number between 1 and 9
     the_number = random.randint(1, 9)
     while game_is_on :
-        #generate random number between 1 and 9
-        print(the_number)
+        if count_the_guesses > 3:
+            print("Sorry , your try is over :(")
+            game_is_on = False
         #ask user to guess the number
+        int_user_guess = None
+        while int_user_guess is None :
+            user_guess = input("Guess the number between 1 to 9! : ")
+            int_user_guess = convert_string_into_int(user_guess)
+        #check if it is same
+        if int_user_guess == the_number:
+            print("Congratulation! You guessed right :)")
+            # then return
+            break
 
         #check answer and compare with the number
-
-        #check if it is same
-        #then return
-
+        if int_user_guess > the_number:
+            # print guess number with upward
+            print("Down the guess :)")
         #else if it is less then the number
-        #print guess number with upward
-
-        #else say lower the guessing number
+        else:
+            # else say lower the guessing number
+            print("Up the guess :)")
+        #count the try of user
+        count_the_guesses += 1
 
 
 
