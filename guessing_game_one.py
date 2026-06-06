2837c683cdc0ac604e10e7ee19058503397d384b
THSPSS <sunyeongbang98@gmail.com>
2026-06-06 22:43:41 +0900

    update gauessing game one with while loop using user input

--- guessing_game_one.py
+++ guessing_game_one.py
@@ -5,35 +5,33 @@ from odd_or_even import convert_string_into_int
 
 def main():
     count_the_guesses = 0
-    game_is_on = True
+    user_guess = None
+    int_user_guess = None
     # generate random number between 1 and 9
     the_number = random.randint(1, 9)
-    while game_is_on :
-        if count_the_guesses > 3:
-            print("Sorry , your try is over :(")
-            game_is_on = False
+    while user_guess != "exit" or int_user_guess != the_number:
+        # if count_the_guesses > 3:
+        #     print("Sorry , your try is over :(")
+        #     game_is_on = False
         #ask user to guess the number
-        int_user_guess = None
         while int_user_guess is None :
             user_guess = input("Guess the number between 1 to 9! : ")
             int_user_guess = convert_string_into_int(user_guess)
-        #check if it is same
-        if int_user_guess == the_number:
-            print("Congratulation! You guessed right :)")
-            # then return
-            break
-
         #check answer and compare with the number
         if int_user_guess > the_number:
             # print guess number with upward
-            print("Down the guess :)")
+            print("Your answer is higher than the number")
         #else if it is less then the number
         else:
             # else say lower the guessing number
-            print("Up the guess :)")
+            print("Your answer is lower than the number")
         #count the try of user
         count_the_guesses += 1
 
+    if user_guess != "exit":
+        print("Congratulation! You guessed right :)")
+        print(f"You tried {count_the_guesses} times")
+    print("===============GAME END=====================")
 
 
 if __name__ == "__main__":
