b5b6b4620b28a38a6fc048fd75b8efc1260a3190
THSPSS <sunyeongbang98@gmail.com>
2026-06-22 21:25:11 +0900

    update check primality function with range starts from 2 to num - 1

--- check_primality_function.py
+++ check_primality_function.py
@@ -4,20 +4,22 @@ def get_integer():
 def is_even(num):
     return num % 2 == 0
 
-def check_primality(num):
+def check_primality():
+    num = get_integer()
     result = []
     if is_even(num):
         return "This is not prime number"
 
-    for i in range(1, num + 1):
+    for i in range(2, num - 1):
+        print(i)
         if num % i == 0:
             result.append(i)
-    if len(result) == 2:
-        return "This is Prime number"
+    if len(result) == 0:
+        return "This is prime number"
     else:
         return "This is not Prime number"
 
 
 if __name__ == "__main__":
     print("init")
-    print(check_primality(3))
+    print(check_primality())
