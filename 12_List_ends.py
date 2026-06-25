99859a1caacc356f9729119d9ed77d2d4d2726d3
THSPSS <sunyeongbang98@gmail.com>
2026-06-25 22:49:10 +0900

    update list ends

---
+++ 12_List_ends.py
@@ -0,0 +1,15 @@
+# Write a program that takes a list of numbers (for examples, a = [5, 10, 15, 20, 25] and makes a new list
+# of only the first and last elements of the given list.
+# For practice, write this code inside a function:
+
+# 1. takes a list of numbers
+# 2. get the first and last element of list
+
+
+def list_ends():
+    #generate random number list
+    print("list ends function")
+
+
+if __name__ == "__main__":
+    list_ends()
