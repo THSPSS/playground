cb730e297537298e1bdc1bbb1827c878154a5201
THSPSS <sunyeongbang98@gmail.com>
2026-06-26 23:19:25 +0900

    update learn about list and properties of lists

--- 12_List_ends.py
+++ 12_List_ends.py
@@ -1,3 +1,4 @@
+import random
 # Write a program that takes a list of numbers (for examples, a = [5, 10, 15, 20, 25] and makes a new list
 # of only the first and last elements of the given list.
 # For practice, write this code inside a function:
@@ -8,7 +9,11 @@
 
 def list_ends():
     #generate random number list
-    print("list ends function")
+    origin_list = random.sample(range(10), 9)
+    last_element = origin_list.pop()
+    new_list = [origin_list[0], last_element]
+    print("new_list : ", new_list)
+    # new_list = [ origin_list(x) if x == 0 for x in range(len(origin_list))]
 
 
 if __name__ == "__main__":
