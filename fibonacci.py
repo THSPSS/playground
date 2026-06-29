5eb8d3451081fffab64257189e1bc1290114ae04
THSPSS <sunyeongbang98@gmail.com>
2026-06-29 21:55:12 +0900

    update fibonacci

--- fibonacci.py
+++ fibonacci.py
@@ -1,6 +1,20 @@
+#fibonacci number
+#Finbonnaci sequence of numbers where the next number in the sequence is the sum of the previous two numbers in the sequence.
+#the sequence looks like this: 1,1,2,3,5,8,13....
 
 def function():
-    print("this is function");
+    times = 0
+    start_number = 1
+
+    while times != 0:
+        times = input("How many Fibonnaci numbers do you want to generate? : ")
+        times = int(times)
+    #현재 주소에서 -1 , -2 한 것의 값을 현재 주소에 집어넣으면 됨
+    #using while loop?
+
+
+
+
 
 
 if __name__ == "__main__":
