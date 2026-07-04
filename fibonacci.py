4faec0b1f33c4e99986192481409cec64679fe9c
THSPSS <sunyeongbang98@gmail.com>
2026-07-05 00:06:12 +0900

    update fibonnaci withfor loop and append list with add current one -1 and current one -2 together

--- fibonacci.py
+++ fibonacci.py
@@ -5,10 +5,21 @@
 def function():
     times = 0
     start_number = 1
+    fibonnaci_list = [start_number, start_number]
 
-    while times != 0:
+    while times == 0:
         times = input("How many Fibonnaci numbers do you want to generate? : ")
         times = int(times)
+
+    if times == 1:
+        print(fibonnaci_list[0])
+    else :
+        for i in range(0, times-2):
+            fibonnaci_list.append(fibonnaci_list[len(fibonnaci_list)-1] + fibonnaci_list[len(fibonnaci_list)-2])
+    print(fibonnaci_list)
+    # get current number from fibonnaci list
+
+    # -1과 -2가 null 값일때는 그냥 + 1 하기
     #현재 주소에서 -1 , -2 한 것의 값을 현재 주소에 집어넣으면 됨
     #using while loop?
 
