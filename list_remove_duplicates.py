e5dd3694092b77443388bd653f50b17608568052
THSPSS <sunyeongbang98@gmail.com>
2026-07-14 23:40:48 +0900

    update list remove duplicate

--- list_remove_duplicates.py
+++ list_remove_duplicates.py
@@ -1,5 +1,10 @@
 
 def loop_with_list(list):
+    new_list = []
+    for i in range(len(list)):
+        if list[i] not in new_list:
+            new_list.append(list[i])
+    print(new_list)
     print("this is functon loop with list")
 
 
@@ -9,7 +14,7 @@ def remove_duplicate_with_set(list):
 
 if __name__ == "__main__":
     print("main")
-    list = [1, 1, 2, 20, 53]
+    list = [1, 1, 2, 4, 20, 53, 20]
     loop_with_list(list)
     remove_duplicate_with_set(list)
 
