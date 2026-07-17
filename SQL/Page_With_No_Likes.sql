a0b9aaa69aee6ff772cd28539285b151b887a9db
THSPSS <sunyeongbang98@gmail.com>
2026-07-18 00:28:02 +0900

    update sql using except in Facebook page likes

--- SQL/Page_With_No_Likes.sql
+++ SQL/Page_With_No_Likes.sql
@@ -1,6 +1,6 @@
---Assume you're given two tables containing data about Facebook Pages and their respective likes (as in "Like a Facebook Page").
---Write a query to return the IDs of the Facebook pages that have zero likes.
---The output should be sorted in ascending order based on the page IDs.
+-- Assume you're given two tables containing data about Facebook Pages and their respective likes (as in "Like a Facebook Page").
+-- Write a query to return the IDs of the Facebook pages that have zero likes.
+-- The output should be sorted in ascending order based on the page IDs.
 
 --------------------------------------------------------------
 
@@ -8,3 +8,11 @@ SELECT pg.page_id
   FROM pages pg
        LEFT JOIN page_likes pl ON pg.page_id = pl.page_id
  WHERE pl.user_id is null;
+
+
+ SELECT page_id
+   FROM pages
+  EXCEPT
+ SELECT page_id
+   FROM page_likes
+ ORDER BY page_id
